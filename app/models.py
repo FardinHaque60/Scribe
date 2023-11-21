05b3b2eb16e4b6e5a2e43e356daa2c0b55454f64
Carlos Quiroz <carlosquiroz@LosMac.local>
2023-11-21 11:24:46 -0800

    added note db model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -7,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    notes = db.relationship('Note', backref="author")
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -16,3 +18,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<user {self.id}: {self.username}>'
+        
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(10000), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeighKey('user.id'))
+    
+    def __repr__(self) -> str:
+        return '<Post {}>'.format(self.body)
