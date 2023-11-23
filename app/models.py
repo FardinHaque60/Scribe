f22d5cb8a71473aa308479265f4eec21b3843a87
Carlos Quiroz <carlosquiroz@LosMac.local>
2023-11-22 19:15:31 -0800

    added db for notes

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=False)
