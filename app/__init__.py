f29e0f58d5a567ea4db65263f8fa9b2e25d21870
Carlos Quiroz <carlosquiroz@LosMac.local>
2023-12-04 21:56:25 -0800

    added code to use drive api

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_login import LoginManager
+from flask_oauthlib.client import OAuth
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 myapp_obj = Flask(__name__)
 login = LoginManager(myapp_obj)
@@ -19,4 +22,16 @@ db = SQLAlchemy(myapp_obj)
 with myapp_obj.app_context():
     from app.models import User, Note
     db.create_all()
+    
+    
+def create_drive_service():
+    CLIENT_SECRET_FILE = 'app/credentials.json'
+    SCOPES = ['https://www.googleapis.com/auth/drive.file']
+    credentials = service_account.Credentials.from_service_account_file(
+        CLIENT_SECRET_FILE, scopes=SCOPES
+    )
+    return build('drive', 'v3', credentials=credentials)
+
+drive_service = create_drive_service()
+
 from app import routes
